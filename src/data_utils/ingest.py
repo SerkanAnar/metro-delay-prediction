fa0d61290c1c619ee0a0a8dd89c235da51f93d2b
Roy Liu <113428371+ruireng@users.noreply.github.com>
2025-12-20 18:02:12 +0100

    Add static data API call function
    Signed-off-by: Roy Liu <113428371+ruireng@users.noreply.github.com>

--- src/data_utils/ingest.py
+++ src/data_utils/ingest.py
@@ -1,4 +1,29 @@
+from dotenv import load_dotenv
 from google.transit import gtfs_realtime_pb2
+import requests
+import os
+
+
+def fetch_static(date, target_dir):
+    """
+        Fetches static planned public transport data using Trafiklab's KoDa API.
+        :date specifies which date the data is fetched from, in YYYY-MM-DD format.
+        :target_dir is the directory that the API output should be saved at, without file name.
+    """
+    load_dotenv()
+    api_key = os.getenv("KODA_API_KEY")
+    
+    url = f'https://api.koda.trafiklab.se/KoDa/api/v2/gtfs-static/sl?date={date}&key={api_key}'
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    file_path = os.path.join(target_dir, f'{date}.zip')
+    
+    with open(file_path, 'wb') as f:
+        f.write(response.content)
+        
+    print(f"Saved GTFS static file to {file_path}.")
 
 
 def pb_to_json(target_dir):
@@ -16,5 +41,6 @@ def pb_to_json(target_dir):
         
 
 # TESTING
-        
+
+fetch_static("2025-12-12", "data")
 # pb_to_json("data/test")
