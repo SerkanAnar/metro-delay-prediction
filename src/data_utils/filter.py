a312f2d629f0e98c2dd6a030564a468f1bf49f93
SerkanAnar <serkan.anar24@gmail.com>
2025-12-24 13:42:22 +0100

    refactor filtering code

--- src/data_utils/filter.py
+++ src/data_utils/filter.py
@@ -7,94 +7,84 @@ from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
 
 
-def filter_routes(original_path, target_path):
+def filter_routes(original_path):
     """
         Filters out non-metro routes from routes.csv and creates a filtered csv file
 
         :param original_path:   Path to the original routes.csv file
-        :param target_path:     Path where the filtered csv should be saved, including filename
         :return:                Set of relevant route ids
     """
 
-    if not os.path.exists(target_path):
-        relevant_line_names = ['Blå linjen', 'Röda linjen', 'Gröna linjen']
-        df = pd.read_csv(original_path, dtype={'route_id':str})
-        filtered_df = df[df['route_long_name'].isin(relevant_line_names)]
-        filtered_df.to_csv(target_path, index=False)
+    relevant_line_names = ['Blå linjen', 'Röda linjen', 'Gröna linjen']
+    df = pd.read_csv(original_path, dtype={'route_id':str})
+    filtered_df = df[df['route_long_name'].isin(relevant_line_names)]
+    filtered_df.to_csv(original_path, index=False)
 
-    df = pd.read_csv(target_path, dtype={'route_id':str})
+    df = pd.read_csv(original_path, dtype={'route_id':str})
     relevant_route_ids = set(df['route_id'])
     return relevant_route_ids
 
 
-def filter_trips(original_path, target_path, relevant_route_ids):
+def filter_trips(original_path, relevant_route_ids):
     """
         Filters out non-relevant trip ids in trips.csv and creates a filtered csv file
 
         :param original_path:       Path to the original trips.csv file
-        :param target_path:         Path where the filtered csv should be saved, including filename
         :param relevant_route_ids:  Set of relevant route ids
         :return:                    Set of relevant shape ids
     """
 
-    if not os.path.exists(target_path):
-        df = pd.read_csv(original_path, dtype={'route_id':str})
-        filtered_df = df[df['route_id'].isin(relevant_route_ids)]
-        filtered_df.to_csv(target_path, index=False)
+    df = pd.read_csv(original_path, dtype={'route_id':str})
+    filtered_df = df[df['route_id'].isin(relevant_route_ids)]
+    filtered_df.to_csv(original_path, index=False)
     
-    df = pd.read_csv(target_path, dtype={'route_id':str, 'shape_id':str})
+    df = pd.read_csv(original_path, dtype={'route_id':str, 'shape_id':str})
     relevant_shape_ids = set(df['shape_id'])
     return relevant_shape_ids
 
 
-def filter_shapes(original_path, target_path, relevant_shape_ids):
+def filter_shapes(original_path, relevant_shape_ids):
     """
         Filters out non-relevant shape ids in shapes.csv and creates a filtered csv file
 
         :param original_path:       Path to the original shapes.csv file
-        :param target_path:         Path where the filtered csv should be saved, including filename
         :param relevant_shape_ids:  Set of relevant shape ids
     """
 
-    if not os.path.exists(target_path):
-        df = pd.read_csv(original_path, dtype={'shape_id':str})
-        filtered_df = df[df['shape_id'].isin(relevant_shape_ids)]
-        filtered_df.to_csv(target_path, index=False)
+    df = pd.read_csv(original_path, dtype={'shape_id':str})
+    filtered_df = df[df['shape_id'].isin(relevant_shape_ids)]
+    filtered_df.to_csv(original_path, index=False)
 
 
-def filter_stop_times(original_path, target_path, relevant_trip_ids):
+def filter_stop_times(original_path, relevant_trip_ids):
     """
         Filters out non-relevant trip ids in stop_times.csv and creates a filtered csv file
 
         :param original_path:       Path to the original stop_times.csv file
-        :param target_path:         Path where the filtered csv should be saved, including filename
         :param relevant_trip_ids:   Set of relevant trip ids
         :return:                    Set of relevant stop ids
     """
 
-    if not os.path.exists(target_path):
-        df = pd.read_csv(original_path, dtype={'trip_id':str})
-        filtered_df = df[df['trip_id'].isin(relevant_trip_ids)]
-        filtered_df.to_csv(target_path, index=False)
+    df = pd.read_csv(original_path, dtype={'trip_id':str})
+    filtered_df = df[df['trip_id'].isin(relevant_trip_ids)]
+    filtered_df.to_csv(original_path, index=False)
     
-    df = pd.read_csv(target_path, dtype={'trip_id':str, 'stop_id':str})
+    df = pd.read_csv(original_path, dtype={'trip_id':str, 'stop_id':str})
     relevant_stop_ids = set(df['stop_id'])
     return relevant_stop_ids
 
 
-def filter_stops(original_path, target_path, relevant_stop_ids):
+def filter_stops(original_path, relevant_stop_ids):
     """
         Filters out non-relevant stop ids in stops.csv and creates a filtered csv file
 
         :param original_path:       Path to the original stops.csv file
-        :param target_path:         Path where the filtered csv should be saved, including filename
         :param relevant_stop_ids:   Set of relevant stop ids
     """
 
-    if not os.path.exists(target_path):
-        df = pd.read_csv(original_path, dtype={'stop_id':str})
-        filtered_df = df[df['stop_id'].isin(relevant_stop_ids)]
-        filtered_df.to_csv(target_path, index=False)
+    df = pd.read_csv(original_path, dtype={'stop_id':str})
+    filtered_df = df[df['stop_id'].isin(relevant_stop_ids)]
+    filtered_df.to_csv(original_path, index=False)
 
 
 def get_trip_ids(path):
@@ -280,31 +270,21 @@ def preprocess_and_aggregate_TU(DATA_ROOT, date, relevant_trip_ids):
         print(f'Preprocessed, filtered and aggregated TU hour {hour} with {len(hourly_snapshots)} snapshots')
 
 
-def filter_static(original_paths, filtered_dir):
+def filter_static(original_paths):
     """
         Runs the full static data filtering pipeline
 
         :param original_paths:  Dictionary mapping filenames to original csv paths
-        :param filtered_dir:    Path to the directory where the filtered csv files are saved
         :return:                Set of relevant trip ids
     """
-
-    routes_output_dir = filtered_dir / "routes.csv"
-    relevant_route_ids = filter_routes(original_paths['routes'], routes_output_dir)
-
-    trips_output_dir = filtered_dir / "trips.csv"
-    relevant_shape_ids = filter_trips(original_paths['trips'], trips_output_dir, relevant_route_ids)
-    
-    relevant_trip_ids = get_trip_ids(trips_output_dir)
-
-    shapes_output_dir = filtered_dir / "shapes.csv"
-    filter_shapes(original_paths['shapes'], shapes_output_dir, relevant_shape_ids)
-
-    stop_times_output_dir = filtered_dir / "stop_times.csv"
-    relevant_stop_ids = filter_stop_times(original_paths['stop_times'], stop_times_output_dir, relevant_trip_ids)
-
-    stops_output_dir = filtered_dir / "stops.csv"
-    filter_stops(original_paths['stops'], stops_output_dir, relevant_stop_ids)
+    print("filtering static data...")
+    relevant_route_ids = filter_routes(original_paths['routes'])
+    relevant_shape_ids = filter_trips(original_paths['trips'], relevant_route_ids)
+    relevant_trip_ids = get_trip_ids(original_paths['trips'])
+    filter_shapes(original_paths['shapes'], relevant_shape_ids)
+    relevant_stop_ids = filter_stop_times(original_paths['stop_times'], relevant_trip_ids)
+    filter_stops(original_paths['stops'], relevant_stop_ids)
+    print("done filtering static data.")
     return relevant_trip_ids
 
 
@@ -319,10 +299,8 @@ def filter_data_for_date(DATA_ROOT, date):
     # Handle static and RT data
     static_directory = DATA_ROOT / "static" / date
     static_original_paths = {f.stem: f for f in static_directory.glob("*.csv")}
-    filtered_dir = static_directory.with_name(static_directory.name + "-filtered")
-    filtered_dir.mkdir(exist_ok=True)
 
-    relevant_trip_ids = filter_static(static_original_paths, filtered_dir)
+    relevant_trip_ids = filter_static(static_original_paths)
     preprocess_and_aggregate_VP(DATA_ROOT, date, relevant_trip_ids)
     preprocess_and_aggregate_TU(DATA_ROOT, date, relevant_trip_ids)
 
