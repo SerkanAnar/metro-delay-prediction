697aeda5b4affdc398e0cc06b12f5656c1d0078b
SerkanAnar <serkan.anar24@gmail.com>
2025-12-29 21:18:53 +0100

    add 1_ingest_realtime.py

---
+++ src/pipeline/1_ingest_realtime.py
@@ -0,0 +1,199 @@
+from src.data_utils.ingest import fetch_realtime_live, fetch_static_live
+import pandas as pd
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+from collections import defaultdict
+from google.transit import gtfs_realtime_pb2
+from google.protobuf.json_format import MessageToDict
+import hopsworks
+from dotenv import load_dotenv
+import os
+import io
+
+
+### This script is run every 15 minutes as a github action
+### 1. Fetches realtime VehiclePositions and TripUpdates data
+### 2. Creates features and uploads them to hopsworks feature group
+### 3. Creates labels and uploads them to hopsworks feature group
+
+
+def fetch_realtime():
+    content_VP = fetch_realtime_live(feed="VehiclePositions")
+    content_TU = fetch_realtime_live(feed="TripUpdates")
+    return content_VP, content_TU
+
+
+def get_delay_lags(fs, line, timestamp):
+    fg = fs.get_feature_group(
+        name="delay features",
+        version=1
+    )
+    df = fg.read()
+    df = df[
+        (df["line"] == line) & (df["timestamp"] < timestamp)
+    ].sort_values("timestamp", ascending=False)
+    
+    return {
+        "delay_15": df.iloc[0]["delay_current"] if len(df) > 0 else None,
+        "delay_30": df.iloc[1]["delay_current"] if len(df) > 1 else None,
+        "delay_45": df.iloc[2]["delay_current"] if len(df) > 2 else None,
+        "delay_60": df.iloc[3]["delay_current"] if len(df) > 3 else None
+    }
+
+
+def extract_current_delay_per_line(content_TU, trip_to_line):
+    latest_delay_per_trip = {}
+    for entity in content_TU.entity:
+        if not entity.HasField("trip_update"):
+            continue
+        trip_update = entity.trip_update
+        trip_id = trip_update.trip.trip_id
+        line = trip_to_line.get(trip_id)
+        if line is None: # In this case, the trip is not relevant (non-metro)
+            continue
+        
+        # Now, we need to find the delay at the latest stop
+        latest_stu = None
+        max_seq = -1
+        for stu in trip_update.stop_time_update:
+            if stu.stop_sequence > max_seq:
+                max_seq = stu.stop_sequence
+                latest_stu = stu
+        if latest_stu is None: 
+            continue
+
+        # Now, we get the actual delay at the latest stop
+        delay = None
+        if latest_stu.HasField("arrival") and latest_stu.arrival.HasField("delay"):
+            delay = latest_stu.arrival.delay
+        
+        elif latest_stu.HasField("departure") and latest_stu.departure.HasField("delay"):
+            delay = latest_stu.departure.delay
+        
+        if delay is not None:
+            latest_delay_per_trip[trip_id] = (line, delay)
+
+    delays_by_line = defaultdict(list)
+    for line, delay in latest_delay_per_trip.values():
+        delays_by_line[line].append(delay)
+
+    avg_delay_by_line = {}
+    for line, delays in delays_by_line.items():
+        avg_delay_by_line[line] = sum(delays) / len(delays) if delays else 0.0
+    
+    return avg_delay_by_line
+
+
+def compute_and_upload_features(content_TU, avg_delay, trip_to_line, fs):
+    now = datetime.now(ZoneInfo("Europe/Stockholm")).replace(second=0, microsecond=0)
+    feature_rows = []
+
+    for line, delay_now in avg_delay.items():
+        lags = get_delay_lags(fs, line, now) # get lagged delays from hopsworks
+
+        feature_rows.append({
+            "timestamp": now,
+            "line": line,
+            "delay_60": lags["delay_60"],
+            "delay_45": lags["delay_45"],
+            "delay_30": lags["delay_30"],
+            "delay_15": lags["delay_15"],
+            "delay_current": delay_now
+        })
+    
+    fg = fs.get_or_create_feature_group(
+        name="delay features",
+        description="lagged time features",
+        version=1,
+        primary_key=["timestamp", "line"]
+    )
+    fg.insert(feature_rows)
+    return feature_rows
+
+
+def compute_and_upload_labels(content_TU, avg_delay, trip_to_line, fs):
+    now = datetime.now(ZoneInfo("Europe/Stockholm")).replace(second=0, microsecond=0)
+    label_rows = [{"timestamp": now, "line": line, "avg_delay": delay} for line, delay in avg_delay.items()]
+
+    # fg = fs.get_or_create_feature_group(
+    #     name="delay labels",
+    #     description="labels for each line",
+    #     version=1, 
+    #     primary_key=["timestamp", "line"]
+    # )
+    # fg.insert(label_rows)
+    return label_rows
+
+
+def load_hopsworks():
+    load_dotenv()
+    hopsworks_key = os.getenv('HOPSWORKS_API_KEY')
+    if hopsworks_key is not None:
+        os.environ['HOPSWORKS_API_KEY'] = hopsworks_key
+
+    # If you are invited to someone else's Hopsworks project, write that project's name below
+    project_name = None
+
+    if project_name:
+        project = hopsworks.login(project=f'{project_name}')
+    else:
+        project = hopsworks.login()
+    fs = project.get_feature_store()
+
+    return project, fs
+
+
+def get_route_name_mapping(routes):
+    relevant = ["Röda linjen", "Blå linjen", "Gröna linjen"]
+    df = pd.read_csv(io.StringIO(routes), dtype={'route_id':str})
+    filtered_df = df[df['route_long_name'].isin(relevant)]
+    return dict(zip(filtered_df['route_id'], filtered_df['route_long_name']))
+
+
+def get_trip_route_mapping(trips, route_ids):
+    mapping = {}
+    df = pd.read_csv(io.StringIO(trips), dtype={'trip_id':str, 'route_id':str})
+    for _, row in df.iterrows():
+        trip_id = row['trip_id']
+        route_id = row['route_id']
+        if route_id in route_ids and trip_id not in mapping:
+            mapping[trip_id] = route_id
+    return mapping
+
+
+def get_trip_to_line():
+    trip_to_line = {}
+    data = fetch_static_live()
+    routes = data["routes.txt"]
+    trips = data["trips.txt"]
+    routes_csv = routes.decode('utf-8')
+    trips_csv = trips.decode('utf-8')
+    
+    route_id_to_name = get_route_name_mapping(routes_csv)
+    route_ids = route_id_to_name.keys()
+    trip_to_route = get_trip_route_mapping(trips_csv, route_ids)
+    for key, value in trip_to_route.items():
+        trip_to_line[key] = route_id_to_name[value]
+    return trip_to_line
+
+
+if __name__ == '__main__':
+    # project, fs = load_hopsworks()
+    trip_to_line = get_trip_to_line()
+
+    content_VP, content_TU = fetch_realtime()
+    avg_delay_by_line = extract_current_delay_per_line(content_TU, trip_to_line)
+    # compute_and_upload_features(content_TU, avg_delay_by_line, trip_to_line, None)
+    label_rows = compute_and_upload_labels(content_TU, avg_delay_by_line, trip_to_line, None)
+
+    print("Outputting results...")
+    print("trip_to_line!")
+    print(f"{trip_to_line.items()}\n")
+
+    print(f"printing avg_delay_by_line...")
+    print(f"{avg_delay_by_line.items()}")
+
+    print(f"printing label rows...")
+    for i, d in enumerate(label_rows):
+        print(f'printing element {i+1} in label rows')
+        print(f'{d.items()}\n')
