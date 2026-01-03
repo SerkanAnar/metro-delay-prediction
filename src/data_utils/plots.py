ff76a08d15c474d6079fcece75d8ae28aef3d10f
Roy Liu <113428371+ruireng@users.noreply.github.com>
2026-01-03 15:11:53 +0100

    Save one file for each line
    Signed-off-by: Roy Liu <113428371+ruireng@users.noreply.github.com>

--- src/data_utils/plots.py
+++ src/data_utils/plots.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
+import os
 import pandas as pd
 
 
 def plot_metro_delay_predictions(df, file_path, hindcast=False):
-    fig, ax = plt.subplots(figsize=(10, 6))
-    
     df = df.copy()
     df['timestamp'] = pd.to_datetime(df['timestamp']).dt.floor('30min')
-    df = df[(df['timestamp'].dt.hour >= 8) & (df['timestamp'].dt.hour <= 24)]
+    df = df[(df['timestamp'].dt.hour >= 8) & (df['timestamp'].dt.hour < 24)]
 
     df = df.sort_values('timestamp')
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
     for line, g in df.groupby('line'):
+        fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(
             g['timestamp'],
             g['predicted'],
@@ -29,12 +29,17 @@ def plot_metro_delay_predictions(df, file_path, hindcast=False):
                 marker='^'
             )
 
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Average delay (seconds)')
-    ax.set_title('Metro delay prediction per line')
-    ax.legend(fontsize='small')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Average delay (seconds)')
+        ax.set_title(f'Metro delay prediction for {line}')
+        ax.legend(fontsize='small')
 
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig(file_path)
-    return plt
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        os.makedirs(f"{file_path}/plots", exist_ok=True)
+        if hindcast:
+            plt.savefig(f"{file_path}/plots/{line.replace(' ', '_')}_hindcast.png")
+        else:
+            plt.savefig(f"{file_path}/plots/{line.replace(' ', '_')}_forecast.png")
+            
+        plt.close(fig)
